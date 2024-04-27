26d3b9c66e9ac01a1729776d01f0878cffaedb4b
Iswar Patel <ispatel@fb.com>
2024-04-27 10:03:22 -0400

    switch to starter for render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,3 +5,9 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
+# Run database migrations
+bundle exec rails db:migrate
+
+# Seed the database
+bundle exec rails db:seed
